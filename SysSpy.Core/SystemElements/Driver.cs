1d919878d0b38d2419922af384bd52daa4a39f99
sergnik97 <sergnik97@gmail.com>
2020-10-26 14:39:01 +0200

    added class for drivers

---
+++ SysSpy.Core/SystemElements/Driver.cs
@@ -0,0 +1,36 @@
+﻿using System.Management;
+
+namespace SysSpy.Core.SystemElements
+{
+    /// <summary>
+    /// Describes driver.
+    /// </summary>
+    class Driver
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Driver"/> class.
+        /// </summary>
+        /// <param name="driver">Driver as <see cref="ManagementObject"/>.</param>
+        public Driver(ManagementObject driver)
+        {
+            DisplayName = driver["DisplayName"].ToString();
+            Name = driver["Name"].ToString();
+            State = driver["State"].ToString();
+        }
+
+        /// <summary>
+        /// Driver's display name.
+        /// </summary>
+        public string DisplayName { get; }
+
+        /// <summary>
+        /// Driver's name.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Driver's state.
+        /// </summary>
+        public string State { get; }
+    }
+}
