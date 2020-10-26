c6bda1e24178d8ed3f64649f99ddd4d158cc8dfc
sergnik97 <sergnik97@gmail.com>
2020-10-26 14:34:09 +0200

    added class for com+ components

---
+++ SysSpy.Core/SystemElements/COMPlusComponent.cs
@@ -0,0 +1,29 @@
+﻿namespace SysSpy.Core.SystemElements
+{
+    /// <summary>
+    /// Describes COM+ component.
+    /// </summary>
+    class COMPlusComponent
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="COMPlusComponent"/> class.
+        /// </summary>
+        /// <param name="name">Component's name.</param>
+        /// <param name="application">Application to which the component belongs.</param>
+        public COMPlusComponent(string name, string application)
+        {
+            Name = name;
+            Application = application;
+        }
+
+        /// <summary>
+        /// Component's name.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Application to which the component belongs.
+        /// </summary>
+        public string Application { get; }
+    }
+}
